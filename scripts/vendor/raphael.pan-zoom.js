1e8d86f45dd4c906d4145d49df576f523b82a6dd
Daan Poron <daan@poron.be>
2013-03-21 15:29:59 +0100

    fixes

--- scripts/vendor/raphael.pan-zoom.js
+++ scripts/vendor/raphael.pan-zoom.js
@@ -1,35 +1,3 @@
-// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
- 
-// requestAnimationFrame polyfill by Erik Möller
-// fixes from Paul Irish and Tino Zijdel
- 
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
-
-
 /**
  * raphael.pan-zoom plugin 0.2.0
  * Copyright (c) 2012 @author Juan S. Escobar
@@ -131,7 +99,9 @@
                 'transform_min_scale': settings.zoomStep,
                 'drag_block_horizontal': true,
                 'drag_block_vertical': true,
-                'transform_always_block': true
+                'transform_always_block': true,
+                'drag_max_touches': 1,
+                'drag_lock_to_axis': false
             });
 
             var initialZoom, previousZoom, previousCenter;
@@ -144,6 +114,12 @@
 
             hammer.on("release", function(event) {
             });
+
+            var $pointer = $('<div style="width: 2px; height: 2px; position: absolute; overflow: hidden; display: box; left: 10px; top: 10px; background-color: red;"></div>');
+            $('body').append($pointer);
+
+            var $pointer2 = $('<div style="width: 2px; height: 2px; position: absolute; overflow: hidden; display: box; left: 10px; top: 10px; background-color: blue;"></div>');
+            $('body').append($pointer2);
 
             var pinching = function(event) {
                 var g = event.gesture,
@@ -162,8 +138,18 @@
                     stepsX = center.pageX - previousCenter.pageX,
                     stepsY = center.pageY - previousCenter.pageY;
 
+                $pointer2.css({
+                    'left': previousCenter.pageX + 'px',
+                    'top': previousCenter.pageY + 'px'
+                });
+
                 move(stepsX, stepsY);
                 previousCenter = center;
+
+                $pointer.css({
+                    'left': center.pageX + 'px',
+                    'top': center.pageY + 'px'
+                });
             };
 
             hammer.on("pinchin", function(event) {
@@ -226,17 +212,27 @@
 
         function applyZoom(val, centerPoint) {
             if (!me.enabled) return false;
+            var previousZoom = me.currZoom;
+
             me.currZoom += val;
-            if (me.currZoom < settings.minZoom) me.currZoom = settings.minZoom;
-            else if (me.currZoom > settings.maxZoom) me.currZoom = settings.maxZoom;
-            else {
-                centerPoint = centerPoint || { x: paper.width/2, y: paper.height/2 };
-
-                deltaX = ((paper.width * settings.zoomStep) * (centerPoint.x / paper.width)) * val;
-                deltaY = (paper.height * settings.zoomStep) * (centerPoint.y / paper.height) * val;
-
-                repaint();
+            if (me.currZoom < settings.minZoom) { 
+                me.currZoom = settings.minZoom;
+            } else if (me.currZoom > settings.maxZoom) {
+                me.currZoom = settings.maxZoom;
             }
+
+            // correct the added value.
+            val = me.currZoom - previousZoom;
+             
+            // if no centerPoint is given ... the center point is at the center of the paper.
+            // the centerPoint is a point given on the original paper width.
+            centerPoint = centerPoint || { x: paper.width/2, y: paper.height/2 };
+
+            // still needs an explanation .. don't know how this works ... but it works :)
+            deltaX = ((paper.width * settings.zoomStep) * (centerPoint.x / paper.width)) * val;
+            deltaY = ((paper.height * settings.zoomStep) * (centerPoint.y / paper.height)) * val;
+
+            repaint();
         }
 
         this.applyZoom = applyZoom;
@@ -246,7 +242,7 @@
             var evt = window.event || e,
                 newPoint = getRelativePosition(evt, container);
 
-            updatePos(newPoint);  // ADDED: Refactored into a separate method to make it reusable       
+            updatePos(newPoint);
 
             repaint();
             me.dragTime++;
@@ -255,7 +251,9 @@
             return false;
         }
 
-        // ADDED: make it possible to move to a certain possition ... this will add a few pixels on the x or y axis
+        /**
+         * Move the viewBox for x number of pixels on the x-axis and y number of pixels on the y-axis
+         */
         function move(x, y) {
             updatePos({
                 'x': initialPos.x + (x),
@@ -267,53 +265,66 @@
 
         this.move = move;
 
-        // ADDED: refactored method to make it reusable
         function updatePos(newPoint) {
-            var newWidth = paper.width * (1 - (me.currZoom * settings.zoomStep)),
-                newHeight = paper.height * (1 - (me.currZoom * settings.zoomStep));
+             // calculate the new width and height ... this is the width/height of what the viewbox will show.
+             // the smaller the width/height, the higher the zoom level (the higher the zoomLevel, the less there is shown in the viewBox)
+            var zoomPercentage = (1 - (me.currZoom * settings.zoomStep)),
+                newWidth = paper.width * zoomPercentage,
+                newHeight = paper.height * zoomPercentage;
 
+            // newPoint and initialPos are points based on the current paper
+            // we need points on the original paper ... when zooming you get more pixels for one pixel on the real paper!
+            // for example original viewbox = 800, the new viewbox = 400 but the viewbox of 400 is still shown in the original paper (800)
+            // so every original pixel of the paper is now 2 pixels in the new viewbox. When you want to calculate the distance between the two points
+            // newPoint and initialPos by doing newPoint - initialPos then you get the distance in regard to the original paper (800) while the viewbox = 600
+            // to get the distance to the current viewbox you need to current pixels / width original paper * width current viewbox.
+            // + viewbox of 800 in a paper of 800 => 1px current viewbox = 1px original paper
+            // + viewbox of 400 in a paper of 800 => 2px current viewbox = 1px original paper
             deltaX = (newWidth * (newPoint.x - initialPos.x) / paper.width) * -1;
             deltaY = (newHeight * (newPoint.y - initialPos.y) / paper.height) * -1;
             initialPos = newPoint;
         }
 
         function repaint() {
-            window.requestAnimationFrame(function() {
-                me.currPos.x = me.currPos.x + deltaX;
-                me.currPos.y = me.currPos.y + deltaY;
+            // me.currPos is the x, y point of the viewBox, based on 100% view of the paper ... we need to add the delta to it.
+            // if there is a delta then the viewBox is moved.
+            me.currPos.x = me.currPos.x + deltaX;
+            me.currPos.y = me.currPos.y + deltaY;
 
-                // ADDED: make sure we don't zoom to far !!
-                var currZoom = (me.currZoom * settings.zoomStep) >= 1 ? me.currZoom -1 : me.currZoom,
-                    zoomPercentage = (1 - (currZoom * settings.zoomStep)),
-                    newWidth = paper.width * zoomPercentage,
-                    newHeight = paper.height * zoomPercentage;
+            var currZoom = (me.currZoom * settings.zoomStep) >= 1 ? me.currZoom -1 : me.currZoom, // make sure we don't zoom to far
+                zoomPercentage = (1 - (currZoom * settings.zoomStep)), // the zooming percentage
+                newWidth = paper.width * zoomPercentage, // this is the new width of what's shown in the viewbox ... this get's smaller when you have a higher zoomLevel (the higher the zoomLevel, the less there is shown in the viewBox)
+                newHeight = paper.height * zoomPercentage; // this is the new height of what's shown in the viewbox ... this get's smaller when you have a higher zoomLevel (the higher the zoomLevel, the less there is shown in the viewBox)
 
-                // make sure you don't pan too far
-                if (me.currPos.x < 0) me.currPos.x = 0;
-                else if (me.currPos.x > (paper.width - newWidth)) { // ADDED changed the if statement
-                    me.currPos.x = paper.width - newWidth;
-                }
+            console.log(zoomPercentage);
 
-                if (me.currPos.y < 0) me.currPos.y = 0;
-                else if (me.currPos.y > (paper.height - newHeight)) { // ADDED changed the if statement
-                    me.currPos.y = paper.height - newHeight;
-                }
+/*
+            // make sure you don't pan too far
+            if (me.currPos.x < 0) me.currPos.x = 0;
+            else if (me.currPos.x > paper.width - newWidth) { // paper.width is not allways the same to the real width and height of the svg ... maybe at custom boundaries?
+                me.currPos.x = paper.width - newWidth;
+            }
 
-                paper.setViewBox(me.currPos.x, me.currPos.y, newWidth, newHeight);
-                settings.onRepaint(); // ADDED call the onRepaint function
-            });
+            if (me.currPos.y < 0) me.currPos.y = 0;
+            else if (me.currPos.y > paper.height - newHeight) {
+                me.currPos.y = paper.height - newHeight;
+            }
+*/
+
+
+            paper.setViewBox(me.currPos.x, me.currPos.y, newWidth, newHeight);
+            settings.onRepaint();
         }
     };
 
     PanZoom.prototype = panZoomFunctions;
 
     function getRelativePosition(e, obj) {
-        var x,y, pos;
+        var x, y, pos;
         if (e.pageX || e.pageY) {
             x = e.pageX;
             y = e.pageY;
-        }
-        else {
+        } else {
             x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
             y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
         }
